b60456605e24fb0942f18fb488c0802d5351ebee
AgustinMaillet <agustinmaillet47@gmail.com>
2023-04-11 04:08:07 -0300

    Autoredirect to http

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+// Redirect all https to http
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] == 'https') {
+        res.redirect('http://' + req.headers.host + req.url);
+    } else {
+        next();
+    }
+});
+
 app.get('*', function(req, res) {
     res.render('index');
     }
